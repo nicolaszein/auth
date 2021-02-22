94c6739b234e4acc9a59e18294f3ad134810fc2e
Nicolas Zein <niczein@gmail.com>
2021-02-22 19:33:05 -0300

    Create user adapter with create method

---
+++ auth/infrastructure/user_adapter.py
@@ -0,0 +1,20 @@
+from auth.infrastructure.entity.user import User
+from auth.infrastructure.password import Password
+from auth.infrastructure.repository.user import UserRepository
+
+
+class UserAdapter:
+
+    def __init__(self):
+        self.__repository = UserRepository()
+        self.__password = Password
+
+    def create(self, full_name, email, password):
+        hashed_password = self.__password.hash_password(password)
+        entity = User(
+            full_name=full_name,
+            email=email,
+            password=hashed_password
+        )
+
+        return self.__repository.create(entity).to_domain()
---
+++ tests/infrastructure/test_user_adapter.py
@@ -0,0 +1,31 @@
+import uuid
+from unittest.mock import MagicMock, patch
+
+from auth.domain.user import User
+from auth.infrastructure.user_adapter import UserAdapter
+
+
+@patch('auth.infrastructure.user_adapter.User')
+@patch('auth.infrastructure.user_adapter.Password')
+@patch('auth.infrastructure.user_adapter.UserRepository')
+def test_create(user_repository_mock, password_mock, user_entity_mock):
+    password = 'a-secret'
+    password_mock.hash_password.return_value = 'hashed_password'
+    user = User(
+        id=uuid.uuid4(),
+        full_name='Foo Bar',
+        email='foo.bar@email.com',
+        password='hashed_password'
+    )
+    user_entity = MagicMock()
+    user_repository_mock().create.return_value = user_entity
+    user_entity.to_domain.return_value = user
+
+    result = UserAdapter().create(full_name='Foo Bar', email='foo.bar@email.com', password=password)
+
+    assert result == user
+    user_entity_mock.assert_called_once_with(
+        full_name='Foo Bar',
+        email='foo.bar@email.com',
+        password='hashed_password'
+    )
